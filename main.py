aaa61198571a2b333219909a285a0d64498c4ded
alim63 <alimk6363@gmail.com>
2024-06-13 18:44:40 +0530

    feat: add make_sandwich function to deduct ingredients and confirm order

--- main.py
+++ main.py
@@ -75,5 +75,7 @@ class SandwichMachine:
     def make_sandwich(self, sandwich_size, order_ingredients):
         """Deduct the required ingredients from the resources.
            Hint: no output"""
+        for ingredient, quantity in order_ingredients.items():
+            self.machine_resources[ingredient] -= quantity
 
 ### Make an instance of SandwichMachine class and write the rest of the codes ###
